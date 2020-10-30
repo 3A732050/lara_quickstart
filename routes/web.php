73ffee252681df894c98b656e9cfe6ef7a935971
林駿綦 <r10101720307@gmail.com>
2020-10-30 23:55:09 +0800

    第一個路由修改為：取得 & 顯示所有的任務

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ use Illuminate\Http\Request;
 
 // 顯示所有任務的清單
 Route::get('/', function () {
-    return view('tasks');
+    return view('tasks', [
+        'tasks' => Task::orderBy('created_at', 'asc')->get()
+    ]);
 });
 
 // 增加新的任務
